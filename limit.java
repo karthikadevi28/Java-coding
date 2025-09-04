5792682a1be5e6e42fea446948b6e429adf8a1e9
karthikadevi28 <karthika24vsb@gmail.com>
2025-09-04 11:28:22 +0530

    Add files via upload

---
+++ limit.java
@@ -0,0 +1,15 @@
+public class limit{
+    public static void main(String[]args){
+        int limit=3;
+        for(int i=1;i<=limit;i++){
+            for(int j=1;j<=limit;j++){
+                for(int k=0;k<=limit;k++){
+                    System.out.println(i+" "+j+" "+k);
+
+
+                }
+            }
+
+        }
+    }
+}
