77f1d3a5d14042e6f91af7bfd48f8736b18fca75
karthikadevi28 <karthika24vsb@gmail.com>
2025-09-17 11:36:58 +0530

    Add files via upload

---
+++ revdigit.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class revdigit {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = scanner.nextInt();
+        int reversed = 0;
+
+        while (number > 0) {
+            int digit = number % 10;      // Extract last digit
+            reversed = reversed * 10 + digit;  // Append digit
+            number /= 10;                 // Remove last digit
+        }
+
+        System.out.println(reversed);
+    }
+}
