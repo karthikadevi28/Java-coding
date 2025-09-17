ea45773b247d1adf2bbdfc115d444fb800195f1a
karthikadevi28 <karthika24vsb@gmail.com>
2025-09-17 11:32:44 +0530

    Add files via upload

--- neon.java
+++ neon.java
@@ -1,22 +1,24 @@
 import java.util.Scanner;
-public class neon{
-    public static void main(String[]args){
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter a number:");
-        int num=sc.nextInt();
-        int square=num*num;
-        int sum=0;
-        while(square>0){
-            int digit=square%10;
-            sum+=digit;
-            square/=10;
-        }
-        if(sum==num){
-            System.out.println(num+"is a Neon Number.");
-        }
-        else{
-            System.out.println(num+"is a Not a Neon Number.");
-        }
 
+public class neon {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int n = scanner.nextInt();
+        int square = n * n;
+        
+        int sum = 0;
+        
+        // Calculate sum of digits of the square
+        while (square > 0) {
+            sum += square % 10;
+            square /= 10;
+        }
+        
+        if (sum == n) {
+            System.out.println("Neon Number");
+        } else {
+            System.out.println("Not Neon Number");
+        }
     }
 }
